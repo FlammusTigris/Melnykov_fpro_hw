e8eb9454478611fe684422fc8150b3157f1eb611
Flammus <flammustigris@gmail.com>
2023-08-28 20:26:21 +0300

    fourth homework

---
+++ js.js
@@ -0,0 +1,78 @@
+///Task1
+let result = '';
+for (let i = 10; i <= 20; i++) {
+    result += i + ', ';
+}
+console.log(result)
+///Task2
+// for (let i = 10; i <= 20; i++) {
+//     console.log(i * i);
+// }
+///Task3
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`7 * ${i} = ${7 * i}`);
+// }
+///Task4
+// let sum = 0;
+// for (let i = 1; i <= 15; i++) {
+//     sum += i;
+// }
+// console.log(sum);
+///Task5
+// let product = 1;
+// for (let i = 15; i <= 35; i++) {
+//     product *= i;
+// }
+// console.log(product);
+///Task6
+// let sum = 0;
+// for (let i = 1; i <= 500; i++) {
+//     sum += i;
+// }
+// console.log(sum / 500);
+///Task7
+// let sum = 0;
+// for (let i = 30; i <= 80; i++) {
+//     if (i % 2 === 0) {
+//         sum += i;
+//     }
+// }
+// console.log(sum);
+///Task8
+// for (let i = 100; i <= 200; i++) {
+//     if (i % 3 === 0) {
+//         console.log(i);
+//     }
+// }
+///Task9
+// const number = prompt('Введіть натуральне число:');
+// for (let i = 1; i <=9; i++) {
+//     if (number % i === 0) {
+//         console.log(i);
+//     }
+// }
+///Task10
+// const number = prompt('Введіть натуральне число:');
+// let count = 0;
+// for (let i = 1; i <=9; i++) {
+//     if (number % i === 0 && i % 2 === 0) {
+//         count++;
+//     }
+// }
+// console.log(count);
+///Task11
+// const number = prompt('Введіть натуральне число:');
+// let sum = 0;
+// for (let i = 1; i <=9; i++) {
+//     if (number % i === 0 && i % 2 === 0) {
+//         sum += i;
+//     }
+// }
+// console.log(sum);
+///Task12
+// for(let i = 1;i <= 10; i++){
+//     console.log(`Multiply for ${i}:`);
+//     for(let j = 1 ; j <= 10 ;j++){
+//         console.log(`${i}* ${j} = ${i*j}`);
+//     }
+// }
