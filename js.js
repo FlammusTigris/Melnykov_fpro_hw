fb17113f42ab027d9d9d08cb4407030e85b7ebe7
Flammus <flammustigris@gmail.com>
2023-09-14 22:11:14 +0300

    ninth homework

--- js.js
+++ js.js
@@ -1,10 +1,20 @@
-let input = document.getElementById('myInput');
-let sideDiv = document.getElementById('sideDiv');
-
-input.addEventListener('focus', function() {
-    sideDiv.style.display = 'block';
-});
-
-input.addEventListener('blur', function() {
-    sideDiv.style.display = 'none';
-});
+const inputField = document.getElementById("myInput");
+const body = document.body;
+function createDiv() {
+    let div = document.createElement("div");
+    div.textContent = "Бу!";
+    div.id = "myDiv";
+    body.appendChild(div);
+    let inputRect = inputField.getBoundingClientRect();
+    div.style.top = inputRect.top + "px";
+    div.style.left = (inputRect.right + 10) + "px";
+    div.style.display = "block";
+}
+function removeDiv() {
+    let div = document.getElementById("myDiv");
+    if (div) {
+        body.removeChild(div);
+    }
+}
+inputField.addEventListener("mouseenter", createDiv);
+inputField.addEventListener("mouseout", removeDiv);
